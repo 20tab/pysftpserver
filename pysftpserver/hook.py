5f174e0dcb7ef2c07dfac9a327c23668fe6cbcdb
Filippo Morelli <filippo@Filippo-Morellis-MacBook-Pro.local>
2016-07-18 15:55:37 -0700

    Edited super for python 2 compatibility

--- pysftpserver/hook.py
+++ pysftpserver/hook.py
@@ -2,7 +2,7 @@
 actions."""
 
 
-class SftpHook:
+class SftpHook(object):
     """A collection of callbacks hooked to specific methods on the server.
 
     Each method is named according to the server method to which it is
